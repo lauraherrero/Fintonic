4a5766de6f81eb4ab3fcbd595b59da38f454b53a
lauraherrero <lauraherrero36@gmail.com>
2020-03-19 17:11:45 +0100

    Fix error in the list component

---
+++ docs/precache-manifest.e7d3d244a033fbc07b217a93c4fb4b4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de651e27e387f0747ee60a7adca35567",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c919a631e520452b824",
+    "url": "./static/css/main.25c67c7e.chunk.css"
+  },
+  {
+    "revision": "65acc90ec91977de66b7",
+    "url": "./static/js/2.d56639ed.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.d56639ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c919a631e520452b824",
+    "url": "./static/js/main.4f6c7a74.chunk.js"
+  },
+  {
+    "revision": "83338be5899897d5f132",
+    "url": "./static/js/runtime-main.13bc43c5.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5301be4c929a9844cc4f16fe25c7554.js"
+  "./precache-manifest.e7d3d244a033fbc07b217a93c4fb4b4a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
